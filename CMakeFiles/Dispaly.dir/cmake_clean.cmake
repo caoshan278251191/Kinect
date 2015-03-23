d05c64fd3c59aafc3bee6b6cf727926f14ed5d49
caoshan <caoshan278251191@gmail.com>
2015-03-23 13:49:44 -0400

    first commint

---
+++ CMakeFiles/Dispaly.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Dispaly.dir/kinectDisplay.cpp.o"
+  "Dispaly.pdb"
+  "Dispaly"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Dispaly.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ CMakeFiles/freenect-regview.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/freenect-regview.dir/regview.c.o"
+  "freenect-regview.pdb"
+  "freenect-regview"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/freenect-regview.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
